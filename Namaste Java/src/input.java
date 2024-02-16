6501c3cba67c753fc59687cab59a8bd86b2c631d
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2024-02-16 15:18:48 +0530

    Add files via upload

---
+++ Namaste Java/src/input.java
@@ -0,0 +1,61 @@
+//import java.util.Scanner;
+//
+//public class input{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("enter the lucky number you want to display");
+//        int num= sc.nextInt();
+//        System.out.println("enter second num");
+//        int num2= sc.nextInt();
+//        System.out.println("the entered number is ="+num+num2);
+//        System.out.println(num+num2);
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class input{
+//    public static void main(String[] args){
+//        Scanner cs = new Scanner(System.in);
+//
+//        System.out.println("enter principle");
+//        float p = cs.nextFloat();
+//        System.out.println("enter rate of interest");
+//        float r = cs.nextFloat();
+//        System.out.println("enter time period ");
+//        int t = cs.nextInt();
+//
+//        float simple_interest= (p*r*t)/100;
+//        System.out.println("cal="+simple_interest);
+//    }
+//}
+
+
+//read character in a JAva
+
+//import java.util.Scanner;
+//public class input {
+//    static public void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("enter char you wanna output ");
+//        char ch= sc.next().charAt(2);
+//        System.out.println("entered char is ="+ch);
+//    }
+//}
+
+//operators in Java
+
+
+public class input{
+    public static void main(String[] args){
+        int p=10, q=20;
+        System.out.println(p+q);
+        System.out.println(q/p);
+        System.out.println(p-q);
+        System.out.println(p%q);
+        System.out.println(p==q);
+        System.out.println(p!=q);
+    }
+}
